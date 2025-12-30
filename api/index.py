fba82443ff2d0f739694159409128a538f1fda21
Honeybee163 <haniarahman635@gmail.com>
2025-12-30 20:23:56 +0500

    --

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ATTENDANCE.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
