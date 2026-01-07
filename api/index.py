d47a3c03a2d80ce0fde33e1b001324bbfb437ba1
Honeybee163 <haniarahman635@gmail.com>
2026-01-07 22:07:57 +0500

    Deploy Django to Vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ATTENDANCE.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "core.settings"   # 🔴 yahan confirm project name
+)
 
 application = get_wsgi_application()
